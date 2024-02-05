7d20f5443fa7a06159348c19028449aac71bb2c5
Uday Raut <126658455+udayraut128@users.noreply.github.com>
2024-02-05 21:04:44 +0530

    Update Sum of two num.c
    Sum of two number in c programming.

--- Sum of two num.c
+++ Sum of two num.c
@@ -1,1 +1,14 @@
+  #include<stdio.h>
+ 
+  int main()
+   {
+    int num1,num2,sum;
+  printf("Enter your first num:");
+  scanf("%d",&num1);
+  printf("Enter your first num:");
+  scanf("%d",&num2);
 
+  sum=num1+num2;
+  printf("Sum of two number is:%d",sum);
+    return 0;
+}
