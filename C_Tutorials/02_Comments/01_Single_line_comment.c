1e870eb66b6c49dcc2c12f246c06fb19941c70dd
coderroot2 <udayraut128@gmail.com>
2024-08-21 14:50:26 +0530

    comment in c

---
+++ C_Tutorials/02_Comments/01_Single_line_comment.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    // This is a single-line comment
+    printf("Hello, World!\n"); // Prints Hello, World! to the console
+    return 0;
+}
---
+++ C_Tutorials/02_Comments/02_Multi-Line_comment.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    /* This is a multi-line comment
+       It can span multiple lines */
+    printf("Hello, World!\n");
+    return 0;
+}
